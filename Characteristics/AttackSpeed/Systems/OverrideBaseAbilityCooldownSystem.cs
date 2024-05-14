92270eddb9b1ed892442bbb01d7e43ea47afe673
Roman <avgustin4ik68@gmail.com>
2024-05-14 15:42:18 +0700

    +attack speed override

---
+++ Characteristics/AttackSpeed/Systems/OverrideBaseAbilityCooldownSystem.cs
@@ -0,0 +1,63 @@
+namespace Game.Ecs.Characteristics.AttackSpeed.Systems
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Ability.Common.Components;
+    using Ability.Components.Requests;
+    using Base.Components;
+    using Base.Components.Requests;
+    using Components;
+    using Cooldown.Components;
+    using Leopotam.EcsLite;
+    using UniGame.Core.Runtime.Extension;
+    using UniGame.LeoEcs.Shared.Extensions;
+    using UniGame.Runtime.ObjectPool.Extensions;
+    using UnityEngine;
+    using UnityEngine.Pool;
+    using UniGame.LeoEcs.Bootstrap.Runtime.Attributes;
+
+    /// <summary>
+    /// Переопределение базового времени восстановления способности на время восстановления атаки
+    /// </summary>
+#if ENABLE_IL2CPP
+    using Unity.IL2CPP.CompilerServices;
+
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+#endif
+    [Serializable]
+    [ECSDI]
+    public class OverrideBaseAbilityCooldownSystem : IEcsInitSystem, IEcsRunSystem
+    {
+        private EcsWorld _world;
+        private EcsFilter _filter;
+        private EcsPool<CreateCharacteristicRequest<AttackSpeedComponent>> _createPool;
+        private EcsPool<BaseCooldownComponent> _baseCooldownPool;
+        private EcsPool<AttackAbilityIdComponent> _attackAbilityIdPool;
+        private EcsPool<SetAbilityBaseCooldownSelfRequest> _setAbilityBaseCooldownPool;
+        public void Init(IEcsSystems systems)
+        {
+            _world = systems.GetWorld();
+            _filter = _world.Filter<CreateCharacteristicRequest<AttackSpeedComponent>>()
+                .End();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var attackCharacteristic in _filter)
+            {
+                ref var request = ref _createPool.Get(attackCharacteristic);
+                if (!request.Owner.Unpack(_world, out var ownerEntity)) continue;
+
+                ref var attackAbilityIdComponent = ref _attackAbilityIdPool.Get(ownerEntity);
+                var slotId = attackAbilityIdComponent.Value;
+
+                ref var abilityBaseCooldownSelfRequest = ref _setAbilityBaseCooldownPool.Get(ownerEntity);
+                abilityBaseCooldownSelfRequest.AbilitySlot = slotId;
+                abilityBaseCooldownSelfRequest.Cooldown = request.Value;
+            }
+        }
+    }
+}
