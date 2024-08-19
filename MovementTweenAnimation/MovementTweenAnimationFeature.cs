980af25aa9332da71b99931c811687ad6dbe4609
Michael <michael_letycev1@mail.ru>
2024-08-19 18:32:57 +0700

    remove new movement animation tween system

--- MovementTweenAnimation/MovementTweenAnimationFeature.cs
+++ MovementTweenAnimation/MovementTweenAnimationFeature.cs
@@ -1,18 +1,22 @@
-﻿namespace MovementTweenAnimation
+﻿namespace Game.Ecs.Movement
 {
     using Cysharp.Threading.Tasks;
+    using global::Animations.Animator.Data;
+    using global::Movement.Systems.NavMesh.Animation;
     using Leopotam.EcsLite;
-    using Systems;
     using UniGame.LeoEcs.Bootstrap.Runtime;
     using UnityEngine;
 
-    [CreateAssetMenu(menuName = "Game/Feature/Movement/Movement Tween Animation Feature")]
+    [CreateAssetMenu(menuName = "Game/Feature/Movement/Movement Tween Animation Feature", 
+        fileName = "Movement Tween Animation Feature")]
     public sealed class MovementTweenAnimationFeature : BaseLeoEcsFeature
     {
+        public AnimationClipId idleClipId;
+        public AnimationClipId walkClipId;
+        
         public override UniTask InitializeFeatureAsync(IEcsSystems ecsSystems)
         {
-            ecsSystems.Add(new MovementTweenAnimatorSystem());
-            
+            ecsSystems.Add(new MovementTweenAnimatorSystem(idleClipId, walkClipId));
             return UniTask.CompletedTask;
         }
     }
--- MovementTweenAnimation/Systems/MovementTweenAnimatorSystem.cs
+++ MovementTweenAnimation/Systems/MovementTweenAnimatorSystem.cs
@@ -1,19 +1,17 @@
-﻿namespace MovementTweenAnimation.Systems
+﻿namespace Movement.Systems.NavMesh.Animation
 {
     using System;
+    using Animations.Animator.Data;
     using Animations.Animatror.Aspects;
-    using Aspects;
-    using Components;
     using Game.Ecs.Characteristics.Speed.Components;
     using Game.Ecs.Core.Components;
-    using Game.Ecs.Movement.Aspect;
     using Game.Ecs.Movement.Components;
     using Leopotam.EcsLite;
+    using UniCore.Runtime.ProfilerTools;
     using UniGame.LeoEcs.Bootstrap.Runtime.Attributes;
-
-    /// <summary>
-    /// Performs movement tween animations for entities.
-    /// </summary>
+    using UniGame.LeoEcs.Shared.Components;
+    using UniGame.LeoEcs.Shared.Extensions;
+    
 #if ENABLE_IL2CPP
     using Unity.IL2CPP.CompilerServices;
 
@@ -28,52 +26,85 @@
         private EcsFilter _filter;
         private EcsWorld _world;
         
-        private MovementTweenAnimationAspect _aspect;
-        private MovementAspect _movementAspect;
-        
-        private AnimationsAnimatorAspect _animatorAspect;
-        private EcsFilter _deathFilter;
+        private EcsPool<SpeedComponent> _speedPool;
+        private EcsPool<AnimatorComponent> _animatorPool;
+        private EcsPool<TransformComponent> _transformPool;
+        private EcsPool<MovementAnimationInfoComponent> _animationInfoPool;
 
+        private AnimationsAnimatorAspect _animatorAspect;
+        
+        private readonly AnimationClipId _idleClipId;
+        private readonly AnimationClipId _walkClipId;
+        
+        private AnimatorsMap _animatorsMap;
+
+        public MovementTweenAnimatorSystem(AnimationClipId idleClipId, AnimationClipId walkClipId)
+        {
+            _idleClipId = idleClipId;
+            _walkClipId = walkClipId;
+        }
+        
         public void Init(IEcsSystems systems)
         {
             _world = systems.GetWorld();
-
             _filter = _world
-                .Filter<MovementTweenAnimationComponent>()
+                .Filter<AnimatorComponent>()
+                .Inc<TransformComponent>()
                 .Inc<MovementAgentComponent>()
                 .Inc<SpeedComponent>()
+                .Inc<MovementAnimationInfoComponent>()
                 .Exc<ImmobilityComponent>()
                 .Exc<PrepareToDeathComponent>()
                 .End();
-
-            _deathFilter = _world
-                .Filter<PrepareToDeathComponent>()
-                .Inc<MovementTweenAnimationComponent>()
-                .End();
+            
+            _animatorsMap = _world.GetGlobal<AnimatorsMap>();
         }
 
         public void Run(IEcsSystems systems)
         {
             foreach (var entity in _filter)
             {
-                ref var speedComponent = ref _aspect.Speed.Get(entity);
+                ref var speedComponent = ref _speedPool.Get(entity);
                 var speed = speedComponent.Value;
-
-                ref var movementTweenAnimationComponent = ref _aspect.MoveAnimation.Get(entity);
-                var scaleTween = movementTweenAnimationComponent.MoveTween;
-
-                if (speed != 0)
-                    scaleTween.Play();
-                else
-                    scaleTween.Reset();
-            }
-
-            foreach (var entity in _deathFilter)
-            {
-                ref var movementTweenAnimationComponent = ref _aspect.MoveAnimation.Get(entity);
-                var scaleTween = movementTweenAnimationComponent.MoveTween;
                 
-                scaleTween.Stop();
+                ref var transform = ref _transformPool.Get(entity);
+                ref var animationInfo = ref _animationInfoPool.Get(entity);
+                ref var animatorComponent = ref _animatorPool.Get(entity);
+                
+                var animator = animatorComponent.Value;
+                var controller = animator.runtimeAnimatorController;
+                if(controller == null) continue;
+                if (animator == null || !animator.isActiveAndEnabled) continue;
+
+                var speedValue = animationInfo.RunSpeed != 0
+                    ? speed / animationInfo.RunSpeed
+                    : 1;
+            
+                animator.speed = speedValue > animationInfo.MaxRunSpeed
+                    ? speedValue / animationInfo.MaxRunSpeed
+                    : 1.0f;
+                
+                if (!_animatorsMap.data.TryGetValue(_idleClipId, out var idleStateData)
+                    || !_animatorsMap.data.TryGetValue(_walkClipId, out var movementStateData))
+                {
+                    GameLog.LogError("AnimatorMapComponent has no such states");
+                    continue;
+                }
+                
+                if (speed != 0)
+                {
+                    if (animator.GetCurrentAnimatorStateInfo(0).shortNameHash !=  movementStateData.stateNameHash)
+                    {
+                        animator.Play(movementStateData.stateNameHash);
+                    }
+                }
+                else
+                {
+                    if (animator.GetCurrentAnimatorStateInfo(0).shortNameHash !=  idleStateData.stateNameHash)
+                    {
+                        animator.Play(idleStateData.stateNameHash);
+                    }
+                }
             }
         }
     }
