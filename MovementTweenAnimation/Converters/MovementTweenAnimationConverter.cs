81d84a674cb6a3f83006ecf5c76b5349fae97b53
Michael <michael_letycev1@mail.ru>
2024-08-19 17:55:11 +0700

    added new functionality in movement tween animation

---
+++ MovementTweenAnimation/Converters/MovementTweenAnimationConverter.cs
@@ -0,0 +1,32 @@
+﻿namespace MovementTweenAnimation.Converters
+{
+    using System;
+    using Components;
+    using Game.Runtime.Tools.Tweens;
+    using Leopotam.EcsLite;
+    using UniGame.LeoEcs.Converter.Runtime;
+    using UniGame.LeoEcs.Shared.Extensions;
+    using UnityEngine;
+
+    /// <summary>
+    /// Converting a GameObject into a MovementTweenAnimationComponent.
+    /// </summary>
+#if ENABLE_IL2CPP
+    using Unity.IL2CPP.CompilerServices;
+
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+#endif
+    [Serializable]
+    public class MovementTweenAnimationConverter : GameObjectConverter
+    {
+        [SerializeField]
+        private ScaleTween movementTween;
+        protected override void OnApply(GameObject target, EcsWorld world, int entity)
+        {
+            ref var movementTweenAnimationComponent = ref world.AddComponent<MovementTweenAnimationComponent>(entity);
+            movementTweenAnimationComponent.MoveTween = movementTween;
+        }
+    }
+}
--- MovementTweenAnimation/MovementTweenAnimationFeature.cs
+++ MovementTweenAnimation/MovementTweenAnimationFeature.cs
@@ -1,22 +1,18 @@
-﻿namespace Game.Ecs.Movement
+﻿namespace MovementTweenAnimation
 {
     using Cysharp.Threading.Tasks;
-    using global::Animations.Animator.Data;
-    using global::Movement.Systems.NavMesh.Animation;
     using Leopotam.EcsLite;
+    using Systems;
     using UniGame.LeoEcs.Bootstrap.Runtime;
     using UnityEngine;
 
-    [CreateAssetMenu(menuName = "Game/Feature/Movement/Movement Tween Animation Feature", 
-        fileName = "Movement Tween Animation Feature")]
+    [CreateAssetMenu(menuName = "Game/Feature/Movement/Movement Tween Animation Feature")]
     public sealed class MovementTweenAnimationFeature : BaseLeoEcsFeature
     {
-        public AnimationClipId idleClipId;
-        public AnimationClipId walkClipId;
-        
         public override UniTask InitializeFeatureAsync(IEcsSystems ecsSystems)
         {
-            ecsSystems.Add(new MovementTweenAnimatorSystem(idleClipId, walkClipId));
+            ecsSystems.Add(new MovementTweenAnimatorSystem());
+            
             return UniTask.CompletedTask;
         }
     }
